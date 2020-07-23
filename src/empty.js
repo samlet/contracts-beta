5f3a1d1088be48f4feebacf8a5c2401306d24f47
Samlet Wu <xiaofei.wu@gmail.com>
2020-07-24 04:07:15 +0800

    init version

---
+++ src/empty.js
@@ -0,0 +1,9 @@
+// src/empty.js
+const Web3 = require('web3');
+const { setupLoader } = require('@openzeppelin/contract-loader');
+
+async function main() {
+    // Our code will go here
+}
+
+main();
